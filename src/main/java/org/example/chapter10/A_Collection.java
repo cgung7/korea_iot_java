1e3eea4ebc64c13359948eabb3ad1b8e55f26a8c
cgung7 <cgung7@naver.com>
2025-07-24 12:16:16 +0900

    20250724

--- src/main/java/org/example/chapter10/A_Collection.java
+++ src/main/java/org/example/chapter10/A_Collection.java
@@ -6,7 +6,7 @@ package org.example.chapter10;
 // : frame(틀) = work(일하다)
 // - 제공받은 일정한 요소와 틀, 규약을 가지고 무언가를 만드는 일
 
-// 1. 컬렌션 프레임워크(Collection Framework)
+// 1. 컬렉션 프레임워크(Collection Framework)
 // : 데이터를 저장하고 처리하는 자료구조의 표준화 된 설계
 // - 다양한 자료구조를 제공, 데이터의 효율적인 관리를 지원
 
@@ -17,7 +17,7 @@ package org.example.chapter10;
 
 // 2) List
 //      : 데이터의 순서가 유지되고 (index 번호), 중복 가능
-//      - 주요 구현제: ArrayList, LinkedList, Vector 등
+//      - 주요 구현체: ArrayList, LinkedList, Vector 등
 
 // 3) Set
 //      : 데이터의 순서유지 X, 중복 불가능 (cf. 특정 구현체-클래스 는 정렬 | 순서 제공 가능)
@@ -33,7 +33,7 @@ package org.example.chapter10;
 
 
 public class A_Collection {
-    //컬렉션 프레임 워크 사용 방법
+    // 컬렉션 프레임 워크 사용 방법
     // - 데이터의 특성에 맞는 클래스를 선택
     // : 중복에 대한 필요성, 순서 유지에 대한 필요성 점검
     // : 키-값 쌍의 데이터에 대한 필요성
